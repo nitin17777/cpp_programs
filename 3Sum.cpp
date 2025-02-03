1101d73cb3121ede08893d48d9acffc44df29686
Nitin Punera <168951296+nitin17777@users.noreply.github.com>
2025-02-03 22:59:03 +0530

    Add files via upload

---
+++ 3Sum.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+    vector<vector<int>> threeSum(vector<int>& nums) {
+
+        //return all the possible triplets of that array which makes upto the sum of 0
+
+        int n = nums.size();
+        sort(nums.begin(),nums.end());
+
+
+
+        
+
+    }
+
+
+int main()
+{
+    return 0;
+
+}
